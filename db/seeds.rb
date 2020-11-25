56344d62d567f0fec3a2cff65525d9e805f206e9
EricCRodriguez <ecrod12@gmail.com>
2020-11-25 14:26:53 -0600

    inital commit/DB/Schema-completed

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create!(name: "Eric", email: "eric@email.com", password: "password")
+User.create!(name: "Peter", email: "peter@email.com", password: "password")
+User.create!(name: "Diana", email: "diana@email.com", password: "password")
